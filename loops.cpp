85d42ccdd8ae38b020252f188825de679eb78274
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-10-06 11:26:41 -0400

    working with loops

---
+++ loops.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    // for (int i = 0; i < 10; i++) {
+    //     cout << "Contador: " << i << endl;
+    // }
+
+    // char option;
+    // cout << "Desea salir del programa? (S) Si : (N) No" << endl;
+    // cin >> option;
+
+    // while (option != 'S') {
+    //     cout << "Desea salir del programa? (S) Si : (N) No" << endl;
+    //     cin >> option;
+    // }
+
+    // cout << "Saliendo del programa" << endl;
+
+    do {
+    } while (false);
+
+    return 0;
+}
